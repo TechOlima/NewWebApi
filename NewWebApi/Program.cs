b45610da4c54f614a5f18d8928c9d42a4ab9cfb7
alexeilapshov <alexeilapshov@yandex.ru>
2023-06-09 12:24:53 +0300

    Добавьте файлы проекта.

---
+++ NewWebApi/Program.cs
@@ -0,0 +1,37 @@
+using System.Text.Json.Serialization;
+using WebApi.Classes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<DataContext>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// подключаем CORS
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.MapControllers();
+
+app.Run();
